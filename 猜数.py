96e4e812073ed94cc94a01d24113395456799542
方高峰 <fanggaofeng@yahoo.com>
2017-06-30 11:33:55 +0800

    Update 猜数.py

--- 猜数.py
+++ 猜数.py
@@ -3,9 +3,9 @@ num=3
 
 print"Guess what number I think?"
 answer=input()
-if answer＜num
+if answer＜num:
  print"too small"
-if answer＞num
+if answer＞num:
  print"too big"
-if answer==num
+if answer==num:
  print"BINGGO"
