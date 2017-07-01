c5f7a419a2e41f4b3492344b8aee9afd88006ad2
方高峰 <fanggaofeng@yahoo.com>
2017-07-02 07:37:22 +0800

    Update guess number 1.2.py

--- guess number 1.2.py
+++ guess number 1.2.py
@@ -1,1 +1,15 @@
-
+#coding=utf-8
+from random import randint
+print"guess me"
+#answer=3
+answer=randint(1,6)
+bingo=False
+while bingo==False:
+  number=input()
+  if answer＞number:
+    print"too small"
+  if answer＜number:
+    print"too big"
+  if answer＝＝number:
+    print"bingo"
+    bingo=Ture
