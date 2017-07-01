5d520ba486c6e10358a27495fabea6beabb394a7
方高峰 <fanggaofeng@yahoo.com>
2017-07-01 10:26:33 +0800

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,12 @@
+#字符串练习
+#coding=utf-8
+print'I said,"I\'m fanggaofeng2017"'
+#I said,"I'm fanggaofeng2017"
+
+print"\\\v//"
+#\\v//
+
+print"Stay hungry,\nstay foolish.\n-Steve Jobs"
+#Stay hungry,
+#stay foolish.
+#-Steve Jobs
