783203fc59d6ff5eda1d00336f143d8c22397250
方高峰 <fanggaofeng@yahoo.com>
2017-07-03 19:51:25 +0800

    Update guess number 1.3.py

--- guess number 1.3.py
+++ guess number 1.3.py
@@ -1,1 +1,22 @@
 #coding=utf-8
+def isEqual(num1,num2):
+  if num1＜num2:
+    print"too small"
+    return False;
+  if num1＞num2:
+    print"too big"
+    return False;
+  if num1＝＝num2:
+    print"bingo"
+    return Ture
+isEqual(1,3)
+isEqual(3,1)
+isEqual(3,3)
+from random import randint
+num=randint(1,3)
+print"guess number"
+bingo=False
+while bingo==False:
+  answer=import()
+  bingo=isEqual
+  
