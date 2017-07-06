60ce3ad18461c9f7197837276b366166fb90e999
fanggaofeng <fanggaofeng@yahoo.com>
2017-07-06 15:15:21 +0800

    Update Tkinder.py

--- Tkinder.py
+++ Tkinder.py
@@ -1,1 +1,22 @@
 # 实现基本GUI功能
+# -*- coding=utf-8 -*-
+from Tkinter import*
+from ScrolledText import ScrolledText
+
+root = Tk()
+root.title('xiaofangsite')
+text = ScrolledText(root,font=("微软雅黑",10))
+text.grid()
+# 标题
+
+button = Button(root,text="bingo",font=("微软雅黑",10),fg="blue")
+button.grid()
+# 按钮
+
+varl = StringVar()
+label = Label(root,font=("微软雅黑",10),fg="red",textvariable=varl)
+label.grid()
+varl.set("loading")
+# 动态文字
+
+root.mainloop()
