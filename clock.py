6694b4a0d358347fede97156e729e25b1f99dc29
fanggaofeng <fanggaofeng@yahoo.com>
2017-07-22 19:59:26 +0800

    Create clock.py

---
+++ clock.py
@@ -0,0 +1,10 @@
+# -*- coding = utf-8 -*-
+import time
+i = 1
+while i:
+    dt = list(time.localtime())
+    hour = dt[3]
+    minute = dt[4]
+    if hour == 19 and minute == 23:
+        print 'bingo'
+        i = 0
