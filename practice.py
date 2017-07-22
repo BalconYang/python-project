c27c37c2e0faee5dc8063eb320e174b0370f8008
fanggaofeng <fanggaofeng@yahoo.com>
2017-07-22 20:05:23 +0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,12 +1,12 @@
-#字符串练习
-#coding=utf-8
+# 字符串练习
+# coding=utf-8
 print'I said,"I\'m fanggaofeng2017"'
-#I said,"I'm fanggaofeng2017"
+# I said,"I'm fanggaofeng2017"
 
 print"\\\v//"
-#\\v//
+# \\v//
 
 print"Stay hungry,\nstay foolish.\n-Steve Jobs"
-#Stay hungry,
-#stay foolish.
-#-Steve Jobs
+# Stay hungry,
+# stay foolish.
+# -Steve Jobs
